02a07511c8a45c627f20aaa081c4eebc3dbdf3ab
alexander3244 <alexander324233@gmail.com>
2017-01-28 18:05:57 +0200

    omap4-common: change ADDITIONAL_DEFAULT_PROPERTIES

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -43,8 +43,10 @@ BOARD_VOLD_MAX_PARTITIONS := 32
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 
 # adb has root
-ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
-ADDITIONAL_DEFAULT_PROPERTIES += ro.allow.mock.location=1
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.secure=0 \
+    ro.allow.mock.location=1 \
+    ro.debuggable=1
 
 # Recovery
 BOARD_HAS_NO_SELECT_BUTTON := true
